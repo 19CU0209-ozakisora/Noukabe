ccc5082e6ea5997f23b85649a5726a8f0465497f
19cu0233masuiyuto <19cu0233@jec.ac.jp>
2021-11-28 21:04:18 +0900

    C++を加えた新しいプロジェクトの追加（4.26.2）

---
+++ Noukabe/Source/Noukabe/Noukabe.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Noukabe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Noukabe, "Noukabe" );
